6d057725af17e6e80e78fcac0f2e6bc4d36bc382
ERIC KELLY <eakelly220@gmail.com>
2018-06-26 22:51:36 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,6 +8,10 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
+<<<<<<< HEAD
     self.artist = Artist.find_or_create_by(name: "Drake")
+=======
+    self.artist.name = "Drake"
+>>>>>>> 26a634b239c2cca97ba981d66a587be8e220386b
   end
 end
